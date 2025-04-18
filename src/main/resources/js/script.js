281e4d4c194392a949fd12129af45fa2134a4072
ArthurAffonso1 <arthuraffonsodejesus15@gmail.com>
2025-04-17 22:18:31 -0300

    atualização 17-04

--- src/main/resources/js/script.js
+++ src/main/resources/js/script.js
@@ -11,3 +11,12 @@ loginBtnLink.addEventListener('click', () => {
     console.log("login")
     wrapper.classList.remove('active');
 });
+
+let button = document.getElementById("button");
+
+button.addEventListener('mousemove', (e) => {
+    x = e.offsetX;
+    y = e.offsetY;
+    button.style.setProperty('--mouse-x', x + "px");
+    button.style.setProperty('--mouse-y', y + "px");
+});
